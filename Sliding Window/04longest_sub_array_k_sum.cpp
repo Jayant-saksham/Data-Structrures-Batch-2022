c1fcb73697fa4549bdf54d60c7d72d32df71bba2
Jayant-saksham <Jayant2410@gmail.com>
2022-05-20 19:33:29 +0530

    Varible sliding window

---
+++ Sliding Window/04longest_sub_array_k_sum.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lenOfLongestArr(int A[], int n, int k) {
+    int maxLen = INT_MIN;
+    int i=0;
+    int j=0;
+    int sum = 0;
+    while(j<n) {
+        sum += A[j];
+        if(sum < k) {
+            j++;
+        }
+        else if(sum == k) {
+            maxLen = max(maxLen, j-i+1);
+            j++;
+        }
+        else if(sum > k) {
+            while(sum > k) {
+                sum -= A[i];
+                i++;
+            }
+            j++;
+        }
+    }
+    return maxLen;
+}
+int main() {
+    int A[] = {-5, 8, -14, 2, 4, 12};
+    int n = sizeof(A)/sizeof(A[0]);
+    int k = -5;
+    cout<<lenOfLongestArr(A, n, k)<<endl;
+    return 0;
+}
